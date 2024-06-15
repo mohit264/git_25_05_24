97a0a75d9735f3864b5254d0769f29f6025245c6
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2024-06-15 10:34:42 +0530

    This file has both registration and login from Merge discussion

---
+++ user_reg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    // Function declaration
+    char* UserRegistration(void);
+
+    printf("Implentation for User Registration ");
+}
+
+char* UserRegistration(void)
+{
+
+}
